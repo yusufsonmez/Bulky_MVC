715c7bca2f2cbf508b97560eb9bfcfc6696b4edd
yusufsonmez <yusufsonmez1995@gmail.com>
2023-10-19 23:57:06 +0300

    Custom and Client side validations added.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -23,6 +23,14 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if(obj.Name == obj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("name","The display order cannot exactly match the Name.");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value.");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
