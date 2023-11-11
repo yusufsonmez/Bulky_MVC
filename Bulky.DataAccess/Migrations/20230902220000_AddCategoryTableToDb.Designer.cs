3cd49760f72f426cc3eb19748a92cb1eb10a83db
yusufsonmez <yusufsonmez1995@gmail.com>
2023-11-12 00:14:35 +0300

    N-Tier Architecture - 2

--- Bulky.DataAccess/Migrations/20230902220000_AddCategoryTableToDb.Designer.cs
+++ Bulky.DataAccess/Migrations/20230902220000_AddCategoryTableToDb.Designer.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace BulkyWeb.Migrations
+namespace BulkyWeb.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
     [Migration("20230902220000_AddCategoryTableToDb")]
